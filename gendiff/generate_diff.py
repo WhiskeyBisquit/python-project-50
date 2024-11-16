a900938602a2b23f0f936f997b1a51a5cab2f3ef
Egor Zimin <ezimine@gmail.com>
2024-11-16 19:41:07 +0700

    update generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -7,18 +7,19 @@ def generate_diff(file_path1, file_path2):
 
     result_str = '{\n'
     for key in file_dict1:
+        key_value1 = f'{key}: {file_dict1[key]}' if key in file_dict1 else ''
+        key_value2 = f'{key}: {file_dict2[key]}' if key in file_dict2 else ''
         if key in file_dict2 and file_dict1[key] == file_dict2[key]:
-            result_str += f'    {key}: {file_dict2[key]}\n'
+            result_str += f'    {key_value2}\n'
         elif key in file_dict2 and file_dict1[key] != file_dict2[key]:
-            result_str += f'  - {key}: {file_dict1[key]}\n'
-            result_str += f'  + {key}: {file_dict2[key]}\n'
+            result_str += f'  - {key_value1}\n'
+            result_str += f'  + {key_value2}\n'
         else:
-            result_str += f'  - {key}: {file_dict1[key]}\n'
+            result_str += f'  - {key_value1}\n'
 
     for el in file_dict2:
         if el not in file_dict1:
             result_str += f'  + {el}: {file_dict2[el]}\n'
 
-    result_str += '}'
-    result = result_str
+    result = result_str + '}'
     return result
