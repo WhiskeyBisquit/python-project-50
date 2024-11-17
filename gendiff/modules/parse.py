c9b57cb5287a8817ddfcbe160b53ece3e9ba1cf0
Egor Zimin <ezimine@gmail.com>
2024-11-17 09:01:02 +0700

    update parse.py

--- gendiff/modules/parse.py
+++ gendiff/modules/parse.py
@@ -1,11 +1,17 @@
 import json
 
 
-def parse(file_path):
-    file_dict = json.load(open(file_path))
-    for el in file_dict:
-        if file_dict[el] is True:
-            file_dict[el] = 'true'
-        elif file_dict[el] is False:
-            file_dict[el] = 'false'
-    return file_dict
+def change_bool(dict_):
+    for el in dict_:
+        if dict_[el] is True:
+            dict_[el] = 'true'
+        elif dict_[el] is False:
+            dict_[el] = 'false'
+    return dict_
+
+
+def parse(file_path1, file_path2):
+    file_dict1 = json.load(open(file_path1))
+    file_dict2 = json.load(open(file_path2))
+
+    return change_bool(file_dict1), change_bool(file_dict2)
