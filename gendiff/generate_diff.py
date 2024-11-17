8c3330b366260caf39f7be7480d07e6fe7cd90a5
Egor Zimin <ezimine@gmail.com>
2024-11-17 10:26:19 +0700

    refactor generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -5,10 +5,13 @@ def get_keys(dict1, dict2):
     keys = list(dict1.keys()) + list(
         filter(lambda x: x not in dict1.keys(), dict2.keys()))
 
-    for n in range(len(keys) - 1, 0, -1):
-        for i in range(n):
-            if keys[i] > keys[i + 1]:
-                keys[i], keys[i + 1] = keys[i + 1], keys[i]
+    for i in range(1, len(keys)):
+        a = keys[i]
+        j = i - 1
+        while j >= 0 and a < keys[j]:
+            keys[j + 1] = keys[j]
+            j -= 1
+        keys[j + 1] = a
 
     return keys
 
