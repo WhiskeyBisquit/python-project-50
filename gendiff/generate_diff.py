5443814da20e71a733d79d819f78d1d59d18ad31
Egor Zimin <ezimine@gmail.com>
2024-11-17 10:05:31 +0700

    refactor generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -4,7 +4,14 @@ from gendiff.modules.parse import parse
 def get_keys(dict1, dict2):
     keys = list(dict1.keys()) + list(
         filter(lambda x: x not in dict1.keys(), dict2.keys()))
-    keys.sort()
+
+    def bubble_sort(arr):
+        for n in range(len(arr) - 1, 0, -1):
+            for i in range(n):
+                if arr[i] > arr[i + 1]:
+                    arr[i], arr[i + 1] = arr[i + 1], arr[i]
+
+    bubble_sort(keys)
     return keys
 
 
