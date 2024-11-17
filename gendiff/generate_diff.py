2f20e160cbff736f0ad5b733af4bd95ac7e8bde3
Egor Zimin <ezimine@gmail.com>
2024-11-17 13:54:50 +0700

    update generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,23 +1,11 @@
 from gendiff.modules.parse import parse
-
-
-def get_keys(dict1, dict2):
-    keys = list(dict1.keys()) + list(
-        filter(lambda x: x not in dict1.keys(), dict2.keys()))
-
-    for i in range(1, len(keys)):
-        a = keys[i]
-        j = i - 1
-        while j >= 0 and a < keys[j]:
-            keys[j + 1] = keys[j]
-            j -= 1
-        keys[j + 1] = a
-
-    return keys
+from gendiff.modules.get_keys import get_keys
 
 
 def generate_diff(file_path1, file_path2):
+    # parse two files into dictionaries
     file_dict1, file_dict2 = parse(file_path1, file_path2)
+    # get list of sorted keys of file_dict1 & file_dict2
     keys = get_keys(file_dict1, file_dict2)
 
     result_str = '{\n'
@@ -26,16 +14,16 @@ def generate_diff(file_path1, file_path2):
         key_value1 = f'{key}: {file_dict1[key]}' if key in file_dict1 else ''
         key_value2 = f'{key}: {file_dict2[key]}' if key in file_dict2 else ''
 
-        if key in file_dict1 and key in file_dict2:
+        if (key in file_dict1) and (key in file_dict2):
             if file_dict1[key] == file_dict2[key]:
                 result_str += f'    {key_value2}\n'
             else:
                 result_str += f'  - {key_value1}\n'
                 result_str += f'  + {key_value2}\n'
-        elif key in file_dict2 and key not in file_dict1:
+        elif (key in file_dict2) and (key not in file_dict1):
             result_str += f'  + {key_value2}\n'
         else:
             result_str += f'  - {key_value1}\n'
 
-    result = result_str + '}'
-    return result
+    result_str += '}'
+    return result_str
