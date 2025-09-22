6642f45e80d84a05bc5b0162c723283a203462fe
Egor Zimin <ezimine@gmail.com>
2025-09-22 19:48:33 +0800

    Update gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -9,6 +9,7 @@ def main():
 
     parser.add_argument("first_file")
     parser.add_argument("second_file")
+    parser.add_argument("-f", "--fomat", dest='FORMAT', type=str, help='set format of output')
 
     args = parser.parse_args()
 
