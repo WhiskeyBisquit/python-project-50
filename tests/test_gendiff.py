0ffb4d9bd57eb718364614e358fedfd0470a9eec
Egor Zimin <ezimine@gmail.com>
2025-09-24 13:31:13 +0800

    Add test_gendiff.py

---
+++ tests/test_gendiff.py
@@ -0,0 +1,24 @@
+import pytest
+
+from gendiff.scripts.gendiff import main
+
+#  data for test gendiff -h --help
+gendiff_help = """[-h] [-f FORMAT] first_file second_file
+
+Compares two configuration files and shows a difference.
+
+positional arguments:
+  first_file
+  second_file
+
+options:
+  -h, --help            show this help message and exit
+  -f FORMAT, --fomat FORMAT
+                        set format of output"""
+
+
+def test_main(capsys):
+    with pytest.raises(SystemExit):
+        main(["--help"])
+    captured = capsys.readouterr()
+    assert gendiff_help in captured.out
