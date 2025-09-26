be4a38303d7990e5da09093a4a25617e344d6c90
Egor Zimin <ezimine@gmail.com>
2025-09-26 15:09:52 +0800

    Update test_data.py

--- tests/test_data/test_data.py
+++ tests/test_data/test_data.py
@@ -1,3 +1,4 @@
+from gendiff.generate_diff import generate_diff
 from gendiff.modules.parse import parse
 
 #  json
@@ -89,3 +90,68 @@ test_variants = [
     (y3, j4, expected_1)
 ]
 
+#  test data for get_stylish function
+expected_2 = """{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}"""
+
+expected_3 = """{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}"""
+
+test_stylish_variants = [
+    (generate_diff(j1, j2), expected_2),
+    (generate_diff(y1, y2), expected_2),
+    (generate_diff(j1, y2), expected_2),
+    (generate_diff(y1, j2), expected_2),
+    (generate_diff(j3, j4), expected_3),
+    (generate_diff(y3, y4), expected_3),
+    (generate_diff(j3, y4), expected_3),
+    (generate_diff(y3, j4), expected_3)
+]
