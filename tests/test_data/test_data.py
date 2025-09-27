946d6489221add8d8d257d969a82cf31e2ec96b8
Egor Zimin <ezimine@gmail.com>
2025-09-27 20:54:34 +0800

    Update test_data.py

--- tests/test_data/test_data.py
+++ tests/test_data/test_data.py
@@ -1,16 +1,8 @@
-from gendiff.generate_diff import generate_diff
-from gendiff.modules.parse import parse
-
 #  json
 file1_json = 'tests/test_data/file1.json'
 file2_json = 'tests/test_data/file2.json'
 file3_json = 'tests/test_data/file3.json'
 file4_json = 'tests/test_data/file4.json'
-
-j1 = parse(file1_json)
-j2 = parse(file2_json)
-j3 = parse(file3_json)
-j4 = parse(file4_json)
 
 #  yaml
 file1_yaml = 'tests/test_data/file1.yaml'
@@ -18,79 +10,8 @@ file2_yaml = 'tests/test_data/file2.yaml'
 file3_yaml = 'tests/test_data/file3.yaml'
 file4_yaml = 'tests/test_data/file4.yaml'
 
-y1 = parse(file1_yaml)
-y2 = parse(file2_yaml)
-y3 = parse(file3_yaml)
-y4 = parse(file4_yaml)
 
-
-#  data for test generate_diff function
-expected = {
-    '- follow': 'false',
-    '  host': 'hexlet.io',
-    '- proxy': '123.234.53.22',
-    '- timeout': 50,
-    '+ timeout': 20,
-    '+ verbose': 'true'
-}
-
-expected_1 = {
-    'common': {
-        '+ follow': 'false',
-        '  setting1': 'Value 1',
-        '- setting2': 200,
-        '- setting3': 'true',
-        '+ setting3': 'null',
-        '+ setting4': 'blah blah',
-        '+ setting5': {
-            'key5': 'value5'
-        },
-        'setting6': {
-            'doge': {
-                '- wow': '',
-                '+ wow': 'so much'
-            },
-            '  key': 'value',
-            '+ ops': 'vops'
-        }
-    },
-    'group1': {
-        '- baz': 'bas',
-        '+ baz': 'bars',
-        '  foo': 'bar',
-        '- nest': {
-            'key': 'value'
-        },
-        '+ nest': 'str'
-    },
-    '- group2': {
-        'abc': 12345,
-        'deep': {
-            'id': 45
-        }
-    },
-    '+ group3': {
-        'deep': {
-            'id': {
-                'number': 45
-            }
-        },
-        'fee': 100500
-    }
-}
-
-test_variants = [
-    (j1, j2, expected),
-    (y1, y2, expected),
-    (j1, y2, expected),
-    (y1, j2, expected),
-    (j3, j4, expected_1),
-    (y3, y4, expected_1),
-    (j3, y4, expected_1),
-    (y3, j4, expected_1)
-]
-
-#  test data for get_stylish function
+#  test data for generate_diff function
 expected_2 = """{
   - follow: false
     host: hexlet.io
@@ -145,13 +66,42 @@ expected_3 = """{
     }
 }"""
 
-test_stylish_variants = [
-    (generate_diff(j1, j2), expected_2),
-    (generate_diff(y1, y2), expected_2),
-    (generate_diff(j1, y2), expected_2),
-    (generate_diff(y1, j2), expected_2),
-    (generate_diff(j3, j4), expected_3),
-    (generate_diff(y3, y4), expected_3),
-    (generate_diff(j3, y4), expected_3),
-    (generate_diff(y3, j4), expected_3)
+expected_4 = """Property 'follow' was removed
+Property 'proxy' was removed
+Property 'timeout' was updated. From 50 to 20
+Property 'verbose' was added with value: true"""
+
+expected_5 = """Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]"""
+
+expected_6 = """gendiff doesn't support 'some_formatter' formatter. 
+Please try 'stylish' or 'plain'"""
+
+test_generate_diff_variants = [
+    (file1_json, file2_json, 'stylish', expected_2),
+    (file1_yaml, file2_yaml, 'stylish', expected_2),
+    (file1_json, file2_yaml, 'stylish', expected_2),
+    (file1_yaml, file2_json, 'stylish', expected_2),
+    (file3_json, file4_json, 'stylish', expected_3),
+    (file3_yaml, file4_yaml, 'stylish', expected_3),
+    (file3_json, file4_yaml, 'stylish', expected_3),
+    (file3_yaml, file4_json, 'stylish', expected_3),
+    (file1_json, file2_json, 'plain', expected_4),
+    (file1_yaml, file2_yaml, 'plain', expected_4),
+    (file1_json, file2_yaml, 'plain', expected_4),
+    (file1_yaml, file2_json, 'plain', expected_4),
+    (file3_json, file4_json, 'plain', expected_5),
+    (file3_yaml, file4_yaml, 'plain', expected_5),
+    (file3_json, file4_yaml, 'plain', expected_5),
+    (file3_yaml, file4_json, 'plain', expected_5),
+    (file1_json, file2_json, 'some_formatter', expected_6)
 ]
