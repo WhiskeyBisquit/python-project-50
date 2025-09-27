2bf65842ad426935c659bf151915e29f1ced4053
Egor Zimin <ezimine@gmail.com>
2025-09-27 20:55:07 +0800

    Update test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,9 +1,10 @@
 import pytest
 
 from gendiff.generate_diff import generate_diff
-from tests.test_data.test_data import test_variants
+from tests.test_data.test_data import test_generate_diff_variants
 
 
-@pytest.mark.parametrize("dict1,dict2,expected", test_variants)
-def test_generate_diff(dict1, dict2, expected):
-    assert generate_diff(dict1, dict2) == expected
+@pytest.mark.parametrize(
+        "file_path1,file_path2,formatter,expected", test_generate_diff_variants)
+def test_generate_diff(file_path1, file_path2, formatter, expected):
+    assert generate_diff(file_path1, file_path2, formatter) == expected
