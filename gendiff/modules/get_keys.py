fcfac9271ca9e4986343f3cf1ab3ba2d52112196
Egor Zimin <ezimine@gmail.com>
2025-11-18 12:22:56 +0700

    update get_keys.py

--- gendiff/modules/get_keys.py
+++ gendiff/modules/get_keys.py
@@ -2,4 +2,4 @@ def get_keys(d1: dict, d2: dict):
     if isinstance(d1, dict) and isinstance(d2, dict):
         return sorted(d1.keys() | d2.keys())
     else:
-        return
+        raise TypeError("non-iterable object")
