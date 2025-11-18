f30253610a46a4c3ace440ea2f7d6e98837406ab
Egor Zimin <ezimine@gmail.com>
2025-11-18 12:13:45 +0700

    update plain.py

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -4,7 +4,7 @@ from gendiff.modules.get_keys import get_keys
 def check_the_value(value):
     if value in ['false', 'true', 'null']:
         return value
-    elif isinstance(value, int):
+    elif isinstance(value, int) or value == 0:
         return value
     else:
         return f"'{value}'"
@@ -33,10 +33,10 @@ def get_plain(d1: dict, d2: dict):  # noqa: C901
                 if d1[key] != d2[key] and not isinstance(d2[key], dict):
                     if isinstance(d1[key], dict):
                         result += f"Property '{new_key}' was updated. From [complex value] to {check_the_value(d2[key])}\n"  # noqa: E501
-                    elif isinstance(d2[key], dict):
-                        result += f"Property '{new_key}' was updated. From {check_the_value(d1[key])} to [complex value]\n"  # noqa: E501
                     else:
                         result += f"Property '{new_key}' was updated. From {check_the_value(d1[key])} to {check_the_value(d2[key])}\n"  # noqa: E501
+                elif d1[key] != d2[key] and isinstance(d2[key], dict) and not isinstance(d1[key], dict):  # noqa: E501
+                    result += f"Property '{new_key}' was updated. From {check_the_value(d1[key])} to [complex value]\n"  # noqa: E501
                 else:
                     walk(d1[key], d2[key])
 
